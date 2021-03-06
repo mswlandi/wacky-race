83e25a6e35975a66fe3ee79ab7bc608bbf4ff04d
gscariott <gringoscariott@gmail.com>
2021-03-06 15:11:48 -0300

    Checkpoint and Laps implementation * Added scripts and a new prefab; * Changed CameraFollow parameters;

---
+++ Assets/Scripts/Laps.cs
@@ -0,0 +1,29 @@
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class Laps : MonoBehaviour {
+     
+     // These Static Variables are accessed in "checkpoint" Script
+    public Transform[] checkPointArray;
+    public static Transform[] checkpointA;
+    public static int currentCheckpoint = 0; 
+    public static int currentLap = 0; 
+    public Vector3 startPos;
+    public int Lap;
+    public int NextCheckpoint;
+     
+    void  Start ()
+    {
+        startPos = transform.position;
+        currentCheckpoint = 0;
+        currentLap = 0;
+    }
+ 
+    void  Update ()
+    {
+        Lap = currentLap;
+        checkpointA = checkPointArray;
+        NextCheckpoint = currentCheckpoint;
+    }
+     
+}
