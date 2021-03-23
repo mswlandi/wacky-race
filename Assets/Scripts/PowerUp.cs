a6e474285346966e57c5a063aa82c817207df057
MSWLandi <38134398+mswlandi@users.noreply.github.com>
2021-03-23 17:14:45 -0300

    power up's lightning rotates and fluctuates

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private float rotateSpeed = 180f;
+    private float floatSpeed = 2f;
+    private float floatAmplitude = 0.3f;
+
+    private Transform lightningTransform;
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightningTransform = this.gameObject.transform.GetChild(0);
+        initialPosition = lightningTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightningTransform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        lightningTransform.position = initialPosition + new Vector3(0, floatAmplitude * Mathf.Sin(floatSpeed * Time.time), 0);
+    }
+}
