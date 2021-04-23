e48935bd9096c8450da1ae88e6e2574158fb5bb5
Henry <henrybkavila@gmail.com>
2021-04-23 01:20:16 -0300

    Add spider with attack and update canvas

--- Assets/Scripts/Powerups/SpiderAttack.cs
+++ Assets/Scripts/Powerups/SpiderAttack.cs
@@ -7,15 +7,43 @@ public class SpiderAttack : MonoBehaviour
     public float radius;
     public float force;
 
+    private Rigidbody rigidbody;
+    private Player player;
+    private SphereCollider collider;
+
+    private bool exploding = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponentInParent<Rigidbody>();
+        player = GetComponentInParent<Player>();
+        collider = GetComponent<SphereCollider>();
+
+        collider.radius = radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown (KeyCode.Space) && isAvailableToAttack(player))
+        {
+            exploding = !exploding;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody otherRigidBody = other.GetComponentInParent<Rigidbody>();
+        if(otherRigidBody != null && exploding)
+        {
+            otherRigidBody.AddExplosionForce(force, player.Position, radius);
+            Debug.Log("explodi");
+        }
+    }
+
+    bool isAvailableToAttack(Player player)
+    {
+        return player.Energy.value == 100;
     }
 }
