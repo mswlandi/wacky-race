7e7b75cd4d70f4d915a142ad955244376bba6c3b
Henry <henrybkavila@gmail.com>
2021-04-23 00:28:31 -0300

    Add spider character

---
+++ Assets/Scripts/Powerups/SpiderAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAttack : MonoBehaviour
+{
+    public float radius;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
