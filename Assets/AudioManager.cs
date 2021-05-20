3775c53d95cbdff95e9f2f5144af9da8cc86bcc0
gscariott <gringoscariott@gmail.com>
2021-05-20 18:49:45 -0300

    Motor sound and audio manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    public void UpdatePitch (string name, float speed)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.pitch = speed;
+    }
+
+}
