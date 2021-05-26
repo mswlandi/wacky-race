76145370cbc4e6bcce2bcf8e7b79fab80d8a487a
Marcos Landi <marcoswinkellandi@gmail.com>
2021-05-26 15:08:29 -0300

    3 seconds still and the car resets

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public abstract class CarController : MonoBehaviour
 {
+    public float TimeStillNeededToReset = 3.0f;
+
     protected Car car;
+
+    private Vector3 lastPosition;
+    private float timeSinceTransformChange = 0;
 
     private void Start()
     {
         car = GetComponent<Car>();
+        lastPosition = car.transform.position;
     }
 
     public virtual void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (timeSinceTransformChange >= TimeStillNeededToReset)
         {
             ResetCar();
         }
+
+        if (lastPosition == car.transform.position)
+        {
+            timeSinceTransformChange += Time.deltaTime;
+        }
+        else
+        {
+            timeSinceTransformChange = 0;
+        }
+
+        lastPosition = car.transform.position;
     }
 
     public void ResetCar()
